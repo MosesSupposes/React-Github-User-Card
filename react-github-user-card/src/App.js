2732014caacb9c779b2e5f0b846c505cd42e06b6
Moses A. Samuel <mosamuel98@gmail.com>
2019-09-03 15:31:24 -0700

    fetch and display user data

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -1,12 +1,33 @@
 import React from 'react'
 import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Github User Card</h1>
-    </div>
-  )
+export default class App extends React.Component {
+  state = {
+    avatar_url: '',
+    bio: '',
+    followers: 0,
+    login: ''
+  }
+
+  componentDidMount() {
+    fetch("https://api.github.com/users/MosesSupposes")
+    .then(res => res.json())
+    .then(res => {
+      this.setState({...res})
+      
+    })
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <h2>{this.state.login}</h2>
+        <img src={this.state.avatar_url} />
+        <p>bio: {this.state.bio}</p>
+        <span>Followers: {this.state.followers}</span>
+      </div>
+    )
+  }
 }
 
-export default App
+
